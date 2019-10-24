86dfc42cc497614df0c402edf51393282fca8225
kendrang <kendrang@users.noreply.github.com>
2019-10-24 15:07:44 -0400

    finished thanks ron

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,73 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    private Character[][] board; // intializing variable
+
     public Board(Character[][] matrix) {
+
+        this.board = matrix;
+
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        if (isHorizontal('X') || (isVertical('X')) || (isDiagonal('X'))) {
+            return true;
+        }
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        if (isHorizontal('O') || (isVertical('0')) || (isDiagonal('0'))) {
+            return true;
+        }
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+        if (!isInFavorOfO() && !isInFavorOfX()) {
+            return true;
+        }
+        return false;
     }
+
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfO()) {
+            return "0";
+        }
+        if (isInFavorOfX()) {
+            return "X";
+        }
+        return " ";
     }
 
+    public Boolean isHorizontal(Character xsAndOs) {
+        for (int i = 0; i <= 2; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+
+    public Boolean isVertical(Character xsAndOs) {
+        for (int i = 0; i <= 2; i++) {
+            if (board[0][i] == board[1][i] && board[i][1] == board[2][i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean isDiagonal(Character xsAndOs) {
+        if ((board[0][0] == xsAndOs && board[1][1] == xsAndOs && board[2][2] == xsAndOs) ||
+                (board)[2][0] == xsAndOs && board[1][1] == xsAndOs && board[0][2] == xsAndOs) {
+            return true;
+        }
+        return false;
+    }
 }
+
